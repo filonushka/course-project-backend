f33f0c4ec8f363a2e6f04aca1b8657c68c47ee40
filonushka <lero4ka.filonova@mail.ru>
2023-01-09 17:00:13 +0300

    feat: change connect

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const DB_NAME = process.env.DB_NAME;
 mongoose
   .connect(
     // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.zubpkej.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-    MONGODB_URL
+    process.env.MONGODB_URL
   )
   .then(() => {
     console.log("Connected to MongoDB");
